86f8551445c605b172a418f10fa394b3389560df
Yehouda Arnauve <yehouda_arnauve@hotmail.com>
2025-06-30 00:50:00 +0200

    last tests for game service

--- domain/src/test/java/com/exercice/services/TennisGameServiceTest.java
+++ domain/src/test/java/com/exercice/services/TennisGameServiceTest.java
@@ -18,6 +18,24 @@ class TennisGameServiceTest {
     }
 
     @Test
+    void should_have_tennis_game_with_random_letter() {
+        TennisGameService service = new TennisGameService();
+
+        String result = service.executeFullGame("JSJSJJ");
+
+        assertThat(result).isEqualTo("Player J WIN !");
+    }
+
+    @Test
+    void should_have_unfinished_tennis_game() {
+        TennisGameService service = new TennisGameService();
+
+        String result = service.executeFullGame("WNN");
+
+        assertThat(result).isEqualTo("Player W : 15 / Player N : 30");
+    }
+
+    @Test
     void should_throw_exception_when_have_bad_entry() {
         TennisGameService service = new TennisGameService();
 
